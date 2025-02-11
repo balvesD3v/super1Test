fd944adb268475ebf3972ce50cae497600545ca6
BalvesD3v <lpbalves7filho@gmail.com>
2025-02-11 13:08:51 -0300

    add styles home and header

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { App } from './App.tsx'
+import GlobalStyles from "./styles/global.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GlobalStyles />
     <App />
   </StrictMode>,
 )
